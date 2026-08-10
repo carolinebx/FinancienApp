067037b1703852f875d3a2cc661d1f3a98c26346
carolinebx <35923080+carolinebx@users.noreply.github.com>
2026-08-10 22:04:22 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Vul deze twee waarden in nadat je de Google Apps Script Web App hebt gedeployed
-// (zie GOOGLE_SHEET_SETUP.md voor de complete stappen).
-
-export const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxbSRp4DZPiGg17Pnp2tTvH7t2EUDCmc4Pwv8Ihhd6aJvrH7EBFcsfB1yIxqUAID0rq/exec";
-export const API_TOKEN = "WachtWoord@DataApp_Financien";
